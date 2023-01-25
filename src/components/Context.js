ddf19e7bccbf0ff394f8c21ce7492e9be06c183a
daniell321123 <jesuscordero341@gmail.com>
2023-01-25 20:09:47 -0300

    commit

---
+++ src/components/Context.js
@@ -0,0 +1,161 @@
+import React, { Component } from 'react'
+
+export const DataContext = React.createContext();
+
+export class DataProvider extends Component {
+
+    state = {
+        products: [
+            {
+                "_id": "1",
+                "title": "Nike Shoes 01",
+                "src": "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/9ee5a3b9-45d2-4f90-83ca-d5487c33d628/calzado-air-jordan-1-zoom-air-comfort-d%C3%ADa-de-muertos-vsg01f.png",
+                "description": "UI/UX designing, html css tutorials",
+                "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+                "price": 23,
+                "colors": ["red", "black", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "2",
+                "title": "Nike Shoes 02",
+                "src": "https://static.nike.com/a/images/t_default/jhrqmz92anpn41uoepjq/calzado-air-max-excee-vl97pm.png",
+                "description": "UI/UX designing, html css tutorials",
+                "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+                "price": 19,
+                "colors": ["red", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "3",
+                "title": "Nike Shoes 03",
+                "src": "https://cf.shopee.com.co/file/188fa4eab3b3cc19b79b3e5e4693760b_tn",
+                "description": "UI/UX designing, html css tutorials",
+                "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+                "price": 50,
+                "colors": ["lightblue", "white", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "4",
+                "title": "Nike Shoes 04",
+                "src": "https://cf.shopee.com.mx/file/ce794ded1baa18c528537a40ebb73633_tn",
+                "description": "UI/UX designing, html css tutorials",
+                "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+                "price": 15,
+                "colors": ["orange", "black", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "5",
+                "title": "Nike Shoes 05",
+                "src": "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/25340310-487f-4816-8887-bca7e49f7019/calzado-air-max-270-Pgb94t.png",
+                "description": "UI/UX designing, html css tutorials",
+                "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+                "price": 10,
+                "colors": ["orange", "black", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "6",
+                "title": "Nike Shoes 06",
+                "src": "https://t1.uc.ltmcdn.com/es/posts/1/8/1/como_lavar_zapatillas_nike_de_cuero_o_gamuza_1181_orig.jpg",
+                "description": "UI/UX designing, html css tutorials",
+                "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+                "price": 17,
+                "colors": ["orange", "black", "crimson", "teal"],
+                "count": 1
+            }
+        ],
+        cart: [],
+        total: 0
+        
+    };
+
+    addCart = (id) =>{
+        const {products, cart} = this.state;
+        const check = cart.every(item =>{
+            return item._id !== id
+        })
+        if(check){
+            const data = products.filter(product =>{
+                return product._id === id
+            })
+            this.setState({cart: [...cart,...data]})
+        }else{
+            alert("The product has been added to cart.")
+        }
+    };
+
+    reduction = id =>{
+        const { cart } = this.state;
+        cart.forEach(item =>{
+            if(item._id === id){
+                item.count === 1 ? item.count = 1 : item.count -=1;
+            }
+        })
+        this.setState({cart: cart});
+        this.getTotal();
+    };
+
+    increase = id =>{
+        const { cart } = this.state;
+        cart.forEach(item =>{
+            if(item._id === id){
+                item.count += 1;
+            }
+        })
+        this.setState({cart: cart});
+        this.getTotal();
+    };
+
+    removeProduct = id =>{
+        if(window.confirm("Do you want to delete this product?")){
+            const {cart} = this.state;
+            cart.forEach((item, index) =>{
+                if(item._id === id){
+                    cart.splice(index, 1)
+                }
+            })
+            this.setState({cart: cart});
+            this.getTotal();
+        }
+       
+    };
+
+    getTotal = ()=>{
+        const{cart} = this.state;
+        const res = cart.reduce((prev, item) => {
+            return prev + (item.price * item.count);
+        },0)
+        this.setState({total: res})
+    };
+    
+    componentDidUpdate(){
+        localStorage.setItem('dataCart', JSON.stringify(this.state.cart))
+        localStorage.setItem('dataTotal', JSON.stringify(this.state.total))
+    };
+
+    componentDidMount(){
+        const dataCart = JSON.parse(localStorage.getItem('dataCart'));
+        if(dataCart !== null){
+            this.setState({cart: dataCart});
+        }
+        const dataTotal = JSON.parse(localStorage.getItem('dataTotal'));
+        if(dataTotal !== null){
+            this.setState({total: dataTotal});
+        }
+    }
+   
+
+    render() {
+        const {products, cart,total} = this.state;
+        const {addCart,reduction,increase,removeProduct,getTotal} = this;
+        return (
+            <DataContext.Provider 
+            value={{products, addCart, cart, reduction,increase,removeProduct,total,getTotal}}>
+                {this.props.children}
+            </DataContext.Provider>
+        )
+    }
+}
